bd2fd48dfe4306d71005dc28d8fac20fd4f5655a
qui000 <150987419+qui000@users.noreply.github.com>
2025-06-21 12:24:03 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
-let breemis = "jumps";
-console.log(breemis);
+let background = document.getElementById("background");
+let click = addEventListener("click",changeBack);
+
+
+//document.getElementById("background").addEventListener("click", changeBack);
+
+background.click;
+
+
+
+function changeBack() {
+    console.log("starting: "+ background.src);
+    if (background.src == "http://127.0.0.1:5500/Background%20Placeholder.png"){
+        background.src = "http://127.0.0.1:5500/garf.jpg";
+        console.log("the 'bckgrn' ran > "+background.src);
+
+
+    }
+    console.log("ending: "+ background.src);
+    
+    
+
+}
